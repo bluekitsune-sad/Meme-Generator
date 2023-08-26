499334ed1741caf72c1cc7b8e4913f1505874b08
BlueKitsune狐 <saadshaan619@gmail.com>
2023-08-27 00:24:57 +0500

    commiting - 8

--- server.py
+++ server.py
@@ -1,13 +1,21 @@
 import http.server
 import socketserver
+import webbrowser
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+        super().end_headers()
 
 # Set the port number to serve the files
 PORT = 8000
 
-# Create a simple HTTP server
-Handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(("", PORT), Handler)
-
-print(f"Serving at http://localhost:{PORT}")
-# Start serving
-httpd.serve_forever()
+# Create a server with the custom request handler
+with socketserver.TCPServer(("", PORT), MyHttpRequestHandler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    # Automatically open the web browser
+    webbrowser.open_new_tab(f"http://localhost:{PORT}")
+    # Start serving
+    httpd.serve_forever()
