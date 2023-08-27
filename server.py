272957d53e60e1c2f7eb6ba2f419a0bfb00f27df
BlueKitsune狐 <saadshaan619@gmail.com>
2023-08-27 16:06:12 +0500

    first commit - 16

--- server.py
+++ server.py
@@ -10,16 +10,6 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
         self.send_header("Expires", "0")
         super().end_headers()
 
-# def reload_page():
-#     with open("index.html", "r") as f:
-#         content = f.read()
-#     content += "\n<script>window.location.reload();</script>\n"
-#     with open("index.html", "w") as f:
-#         f.write(content)
-
-# def start_timer():
-#     threading.Timer(10, reload_page).start()
-
 if __name__ == "__main__":
     # Set the port number to serve the files
     PORT = 8000
@@ -30,9 +20,6 @@ if __name__ == "__main__":
         
         # Open the default web browser
         webbrowser.open_new_tab(f"http://localhost:{PORT}")
-        
-        # Start the timer to reload the page
-        # start_timer()
         
         # Start serving
         httpd.serve_forever()
